264fe108e95edb469e9254cf49c04126b0fdad69
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-30 00:29:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,7 +42,7 @@ exports.respond = function(botRoom) {
     isMod:        mods.isMod(request.user_id),
     bots:         rooms.getRooms(),
     funMode:      sysCommands.fun_mode(),
-    owner:        config.env().owner
+    owner:        config.owner //config.env().owner
   };
 console.log(request);
   console.log('-----');
