6db3b70b8144fddab7c4123111a7535354afb1bd
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-30 01:11:47 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -41,7 +41,7 @@ exports.getAllDocuments = function(collection, callback) {
 //});
     var allDocs = collection.find().toArray(function(err, docs) {
       callback(docs);
-     console.log(docs);
+     console.log(allDocs);
     //  db.close();
       client.close();
     });
