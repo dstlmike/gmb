e90bcc1dfa1f4f43cdc336aed022aa284f86310d
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-30 02:12:52 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -40,7 +40,7 @@ exports.getAllDocuments = function(collection, callback) {
   var collection = client.db("sampledb").collection("test"); // perform actions on the collection object 
   //client.close();
 //});
-    var allDocs = collection.find().forEach(client => console.log(client.name) { //(function(err, docs) {
+    var allDocs = collection.find().forEach(client => console.log(client.name)) { //(function(err, docs) {
       callback(docs);
      console.log(collection.find({}));
      // console.log(docs);
@@ -59,6 +59,7 @@ exports.findDocs = function(collection, matchHash, callback) {
     cursor.each(function(err, doc){
       if(doc != null)
         ret.push(doc);
+  console.log(cursor);
       else
         callback(ret);
     });
