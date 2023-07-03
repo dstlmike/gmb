c83307b542b07a1ae09536e446b001a0808624ab
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-07-03 03:45:12 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -186,7 +186,7 @@ run().catch(console.dir);
 
 
 var mongoDB     = require('mongodb').MongoClient;
-//var db = require('mongodb').Db
+var db = require('mongodb').Db
 
 //var connection_string = 'mongodb://dstl%5Fmike1%40hotmail%2Ecom:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
