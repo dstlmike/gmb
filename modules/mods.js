1bd11e4310ff87e1260f28915b60532937be1def
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-07-02 23:40:23 -0400

    Update mods.js

--- modules/mods.js
+++ modules/mods.js
@@ -2,7 +2,7 @@ var mods = [];
 var db = require('./db.js');
 var db_table = 'mods';
 var modCommands = [addModCmd, listModsCmd, removeModCmd];
-var config = require('../config/config.js');
+
 getAllMods();
 exports.modName = "Mod Control";
 
@@ -95,10 +95,10 @@ function addModCmd(request, owner, callback) {
   var regex = /^\/mod add (.+?) ([\s\S]+)/i;
 
   if (regex.test(request.text)) {
-  //  if (request.user_id != owner.id) {
-     // callback(true, "You wish you could add mods", []);
-      //return "You wish you could add mods.";
-   // }
+    if (request.user_id != owner.id) {
+      callback(true, "You wish you could add mods", []);
+      return "You wish you could add mods.";
+    }
 
     if (!request.attachments[0].user_ids){
       callback(true, "You have to mention the user you want to mod using @");
@@ -112,7 +112,7 @@ function addModCmd(request, owner, callback) {
     } else {
       var newMod = {name: val[1], id: val[2]};
       addModToDB(newMod);
-     // mods.push(newMod);
+      mods.push(newMod);
       callback(true, val[1] + " is now a mod.", []);
     }
   } else {
