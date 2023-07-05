3fe1948c5a20817a843b728ce22de98ea8f19d0c
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-07-05 03:03:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ server = http.createServer(function (req, res) {
   });
 });
 
-port =  "8080"; //process.env.PORT || 8080;
+port =  8080; //process.env.PORT || 8080;
 ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 server.listen(port);
